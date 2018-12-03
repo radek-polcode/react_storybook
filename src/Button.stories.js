263263d3cd23559adf1c1097d5b51bd8d17d4825
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-03 12:02:28 +0100

    Add info to button storybook

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { wInfo } from '../src/utils.js';
 
 import { storiesOf } from '@storybook/react/dist/client/preview';
 import { Button } from './Button';
 
-storiesOf('Button', module).addWithJSX('with background', () => (
-  <Button bg="palegoldenrod">Hello World</Button>
-)).addWithJSX('with background 2', () => (
-  <Button bg="green">Hello World</Button>
-))
+storiesOf('Button', module)
+  .addWithJSX(
+    'with background', 
+    wInfo(`
+      description of the component
+
+      ~~~js
+      <Button>Click here</Button>
+      ~~~
+  `)(() => (
+    <Button bg="palegoldenrod">Hello World</Button>
+  )))
+  .addWithJSX('with background 2', () => (
+    <Button bg="green">Hello World</Button>
+  ));
